b6b2c7340ef4e3a09c6b761c5f338a6a2e41da84
stacysorey <stacysorey@gmail.com>
2020-12-11 05:50:01 -0500

    commit tree 0eade9cae7e6fbfc3d555e3baa12da0709d4ef98

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, 'Mr. Chocolate', 1, 'M', 'Brown', 'Fuzzy', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, 'Rowdy', 1, 'M', 'Black', 'Fuzzy', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, 'Tabitha', 1, 'F', 'White', 'Fuzzy', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, 'Sergeant Brown', 1, 'M', 'Brown', 'Fuzzy', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, 'Melissa', 1, 'F', 'Brown', 'Fuzzy', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, 'Grinch', 1, 'M', 'Green', 'Grouchy', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, 'Wendy', 1, 'F', 'Light Brown', 'Sweet', 1);
+INSERT INTO bears (id, age, sex, color, temperament, alive) VALUES (8, 1, 'M', 'Black', 'Mean', 0);      
